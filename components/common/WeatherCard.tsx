1ca498de5cfebfd7f8a71f8bbbfd63d8e4b3dd41
whygee-dev <76694551+whygee-dev@users.noreply.github.com>
2022-05-12 00:07:02 +0200

    👌 IMPROVE: Bump NextJS version & fix subsequent css bug

--- components/common/WeatherCard.tsx
+++ components/common/WeatherCard.tsx
@@ -14,7 +14,11 @@ const WeatherCard: React.FC<Props> = (props: Props) => {
   return (
     <section className={styles.container}>
         <h3>{props.day}</h3>
-        <Image src={`/${props.iconName}.svg`} alt="Weather State" layout="responsive" height={100} width={100} />
+        
+        <div className={styles.imageContainer}>
+          <Image src={`/${props.iconName}.svg`} alt="Weather State" layout="responsive" height={100} width={100} />
+        </div>
+
         <div className={styles.minMaxTemp}>
             <h4 className={styles.max}>{`${props.max}°${props.unit}`}</h4>
             <h4 className={styles.min}>{`${props.min}°${props.unit}`}</h4>
